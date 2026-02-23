13a0de1e6c0172a6be532ff05ba9d9003910f435
rashedku <rasheduzzamanr5@yahoo.com>
2026-02-23 13:16:41 +0000

    Fix CORS for frontend connection

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,25 @@ require('dotenv').config();
 
 const app = express();
 
+const allowedOrigins = [
+  'https://rasheduzzaman-portfolio-kutm.vercel.app',
+  'http://localhost:3000'
+];
+
 app.use(cors({
-  origin: '*'
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+
+app.options('*', cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
